639350bec37b1a06665300d9115f497ff2f85c25
maerzhase <markus@lili16.de>
2018-02-15 12:05:27 +0100

    fix isSoundcloudURL export

--- packages/string-utils/src/validation/isSoundcloudURL.js
+++ packages/string-utils/src/validation/isSoundcloudURL.js
@@ -1,4 +1,6 @@
 //FIXME: write tests and make proper regex for testing for soundcloud provider
-export function isSoundcloud(url) {
+const isSoundcloudURL = (url) => {
   return url.indexOf('soundcloud') > -1 && isUrl(url);
 }
+
+export default isSoundcloudURL;
