99331c57906c42ac6c2d03a333eb81f25435a488
maerzhase <markus@lili16.de>
2018-02-15 12:08:46 +0100

    adds checkup for URL in isSoundcloudURL

--- packages/string-utils/src/validation/isSoundcloudURL.js
+++ packages/string-utils/src/validation/isSoundcloudURL.js
@@ -1,6 +1,8 @@
+import isURL from './isURL';
+
 //FIXME: write tests and make proper regex for testing for soundcloud provider
 const isSoundcloudURL = (url) => {
-  return url.indexOf('soundcloud') > -1 && isUrl(url);
+  return url.indexOf('soundcloud') > -1 && isURL(url);
 }
 
 export default isSoundcloudURL;
