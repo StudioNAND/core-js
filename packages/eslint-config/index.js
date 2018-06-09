1dc2425960ffc99ede69b6906b98c2ea3a08c6e3
maerzhase <markus@lili16.de>
2018-06-09 11:53:37 +0200

    remove airbnb config from eslintconfig

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,47 +1,57 @@
 /* eslint-disable quote-props */
 module.exports = {
-  parser: 'babel-eslint',
-  extends: 'airbnb',
-  env: {
-    browser: true,
-    node: true,
-    jest: true,
-    jasmine: true,
+  "parser": "babel-eslint",
+  "extends": [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:react/recommended"
+  ],
+  "plugins": [
+    "import",
+    "react",
+    "jsx-a11y"
+  ],
+  "env": {
+    "browser": true,
+    "node": true,
+    "jest": true,
+    "jasmine": true,
+    "es6": true
   },
-  rules: {
-    'react/jsx-filename-extension': [
-      'warn',
+  "rules": {
+    "react/jsx-filename-extension": [
+      "warn",
       {
-        extensions: [
-          '.js',
-          '.jsx',
-        ],
-      },
+        "extensions": [
+          ".js",
+          ".jsx"
+        ]
+      }
     ],
-    'no-multi-spaces': [
-      'error',
+    "no-multi-spaces": [
+      "error",
       {
-        exceptions: {
-          ImportDeclaration: true,
-          VariableDeclarator: true,
-        },
-      },
+        "exceptions": {
+          "ImportDeclaration": true,
+          "VariableDeclarator": true
+        }
+      }
     ],
-    'no-underscore-dangle': 'off',
-    'react/forbid-prop-types': 'off',
-    'react/no-unused-state': 'warn',
-    'no-param-reassign': [
-      'error',
+    "no-underscore-dangle": 0,
+    "react/forbid-prop-types": 0,
+    "no-param-reassign": [
+      "error",
       {
-        props: false,
-      },
+        "props": false
+      }
     ],
-    'class-methods-use-this': 'off',
-    'max-len': 'off',
-    'jsx-a11y/no-static-element-interactions': 'off',
-    'react/no-unused-prop-types': 'off',
+    "class-methods-use-this": 0,
+    "max-len": 0,
+    "jsx-a11y/no-static-element-interactions": 0,
+    "react/no-unused-prop-types": 0
   },
-  globals: {
-    '__DEV__': true,
-  },
+  "globals": {
+    "__DEV__": true
+  }
 };
