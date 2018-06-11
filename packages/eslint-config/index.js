fa9634a59b7f6d3bd7821256a8788bf9ad8c2a15
maerzhase <markus@lili16.de>
2018-06-11 09:20:12 +0200

    update eslint config

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -20,38 +20,24 @@ module.exports = {
     "es6": true
   },
   "rules": {
-    "react/jsx-filename-extension": [
-      "warn",
-      {
-        "extensions": [
-          ".js",
-          ".jsx"
-        ]
-      }
-    ],
-    "no-multi-spaces": [
-      "error",
-      {
-        "exceptions": {
-          "ImportDeclaration": true,
-          "VariableDeclarator": true
-        }
-      }
-    ],
     "no-underscore-dangle": 0,
     "react/forbid-prop-types": 0,
-    "no-param-reassign": [
-      "error",
-      {
-        "props": false
-      }
-    ],
     "class-methods-use-this": 0,
     "max-len": 0,
     "jsx-a11y/no-static-element-interactions": 0,
-    "react/no-unused-prop-types": 0
+    "react/no-unused-prop-types": 1,
+    "eol-last": 2,
+    "no-multiple-empty-lines": [
+      2,
+      {
+        "max": 2,
+        "maxEOF": 1,
+        "maxBOF": 0,
+      }
+    ],
+    "no-console": 1,
   },
   "globals": {
     "__DEV__": true
   }
-};
+}
